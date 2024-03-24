608c52b3f6c5cd2a2d0d2d0150e2dad8e83b7d21
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-24 23:07:52 +0100

    Enum with types of products that can be stored in the refrigerated container is created

---
+++ Tutorial2/Tutorial2/Product.cs
@@ -0,0 +1,15 @@
+namespace Tutorial2;
+
+public enum Product
+{
+    Bananas,
+    Chocolate,
+    Fish,
+    Meat,
+    IceCream,
+    FrozenPizza,
+    Cheese,
+    Sausages,
+    Butter,
+    Eggs
+}
