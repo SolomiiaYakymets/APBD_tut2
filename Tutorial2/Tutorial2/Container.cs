17bb409c35a0264efc25ff21b5b053ff2be6d555
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-25 23:55:44 +0100

    Container class is modified

--- Tutorial2/Tutorial2/Container.cs
+++ Tutorial2/Tutorial2/Container.cs
@@ -7,17 +7,15 @@ public abstract class Container
     public double Weight { get; set; }
     public double Depth { get; set; }
     public double MaxPlayload { get; set; }
-    public int Count { get; set; }
     public string SerialNum { get; set; }
 
-    public Container(double mass, double height, double weight, double depth, double maxPlayload, string serialNum )
+    public Container(double mass, double height, double weight, double depth, double maxPlayload)
     {
         Mass = mass;
         Height = height;
         Weight = weight;
         Depth = depth;
         MaxPlayload = maxPlayload;
-        SerialNum = serialNum;
 
     }
     
